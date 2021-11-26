c9f5aad60f101de9a0b9d6c18bb907b4356467ff
GiorgiKumelashvili <giorgi.kumelashvili21@gmail.com>
2021-11-26 18:45:58 +0400

    handlebars all basic utilities added

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,6 +6,28 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @Render('index')
+  index() {
+    return {
+      first: 'initial text',
+
+      firstname: 'Yehuda',
+      lastname: 'Katz',
+
+      people: [
+        {
+          firstname: 'Nils',
+          lastname: 'Knappmeier',
+        },
+        {
+          firstname: 'Yehuda',
+          lastname: 'Katz',
+        },
+      ],
+    };
+  }
+
+  @Get('/api')
   getHello(): string {
     return this.appService.getHello();
   }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 import { AppModule } from './app.module';
+import * as hbs from 'hbs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
+
+  hbs.registerPartials(join(__dirname, '..', 'views', 'layouts'));
+
+  hbs.handlebars.registerHelper('helper_name', () => 'helper value');
+  hbs.handlebars.registerHelper('loud', (aString) => aString.toUpperCase());
+  hbs.handlebars.registerHelper('print_person', function () {
+    return this.firstname + ' ' + this.lastname;
+  });
+
   await app.listen(3000);
 }
 bootstrap();
